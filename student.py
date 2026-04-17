bfd147af877a956dc9407561c3fc618711fce495
Venky <pativenky448@gmail.com>
2026-04-17 13:06:52 +0530

    MINI ATM PROJECT

--- student.py
+++ student.py
@@ -1,50 +1,59 @@
-class Student:
-    roll_number = 50
-    def __init__(self,elgibility):
-        
-        self.elgibility= elgibility
-    def check (self): 
-        score = int(input("enter your score:"))
-        if score  <= self.elgibility :
-            print("Congrates your are elgible for this collge \n let move to next step " )
-            print("grand welcome to our siddartha  institue of engineeringa and technology")
-            return True
-        else:
-            print("you are not eligibil for this university try to move with other one ")
-            return False
 
-    def branch_select(self):
-        self.user_branch = input ("enter which branch are you willing to study here:").upper()
-        branch={ 'ECE':50000, 'EEE':48000,'MECH':45000,'CIVIL':44000,}
-        if self.user_branch in branch:
-            self.fees = branch[self.user_branch]
-            print(f"you choose {self.user_branch} and it fees was {self.fees}")
-            return True
-        else:
-            print("please enter a valid branch \n you enter invalid branch ")
-            return False
+class Bank:
+    def __init__(self,amount):
+      self.amount= amount
+      self.deposit = 0
+      self.amount_withdraw = 0
 
-    def _details_entering(self):
-        Student.roll_number +=1 
-        self.roll_no = Student.roll_number
-        self.name = input("Enter your name :").upper()
-        self.date_of_birth = input("enter your date of birth:")
+    def save_data(self):
+     with open("data.txt", "w") as f:
+        f.write(str(self.amount))
+    
+    def check_authenticate(self):
+       
+       self.pass_word = input("enter your 8 digits  pin:")
+       if self.pass_word == "27018516":
+           print("yeh you have to seccessfully logined ")
+           return True
+       else:
+           print("invalid pin")
+           return False
 
-        print("your registration was successfully completed")
-        
-    def id_details(self):
-        print("<<< ID DETAILS >>>\n")
-        print(f"NAME = {self.name}")
-        print(f"BRANCH = {self.user_branch}")
-        print(f"DOB = {self.date_of_birth}")
-        print(f"ROLL.NO={self.roll_no}")
-               
-               
+    def _deposit(self):
+        self.deposit = int( input("how much money you want to deposit :"))
+        self.amount += self.deposit
+        self.save_data()
+        print(f"you deposited{self.deposit} and you  balance was {self.amount}")
+        return self.amount  
 
-s1= Student(500)
-if s1.check():
- if s1.branch_select():
-   s1._details_entering()
-   s1.id_details()
+    def withdraw (self):
+       self.amount_withdraw =int(input("how much money you want to withdraw: "))
+       if self.amount >= self.amount_withdraw:
+            self.amount  -= self.amount_withdraw
+            self.save_data()
+            print(f"you withdraw the {self.amount_withdraw}\n remaining balance {self.amount}")
+       else:
+           print("insufficient funds")
+
+    def run(self):
+        if not self.check_authenticate():
+            return 
+        while True:
+            print("\n1.Deposit 2.Withdraw 3.Balance 4.Exit ")
+            choice = input("Enter your choice:")
+            if  choice == "1":
+                self._deposit()
+            elif choice == "2":
+                self.withdraw()
+            elif choice == "3":
+                print(f"{self.amount}")
+            elif choice == "4":
+                print("Thank you")
+                break
+            else:
+                print("Invalid option ")
 
 
+s1 = Bank(0)
+s1.run()
+ 
